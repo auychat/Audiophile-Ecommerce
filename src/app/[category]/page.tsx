805429ad9867f9d440ecb7b7c83434cd1b7c48cf
auychat <auychatcharin@gmail.com>
2023-09-20 14:34:47 +0630

    99% Finished Category Page Responsive (Updated)
    - In the previous commit, I finished the mobile version but forgot about other devices.

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -51,27 +51,29 @@ const Category: React.FC<pageProps> = ({ params }) => {
             key={product.item.id}
             className={`max-w-[1110px] min-h-[560px] mx-auto pt-[10rem] flex flex-row ${
               product.reverse && "flex-row-reverse"
-            } gap-[125px] justify-center items-center xs:w-[91.75%] xs:pt-16 xs:flex-col xs:gap-8 xs:pb-[58px]`}
+            } gap-[125px] justify-center items-center xs:w-[91.75%] xs:pt-16 xs:flex-col xs:gap-8 xs:pb-[58px] sm:w-[91.5%] sm:flex-col sm:gap-8 sm:pt-[120px] md:w-[91.5%] md:gap-8 md:pt-[120px] lg:w-[91.5%]`}
           >
-            <Image
-              src={product.item.image.desktop}
-              alt={product.item.name}
-              width={540}
-              height={560}
-              // sizes="100vw"
-              className="w-[540px] h-[560px] rounded-lg xs:max-h-[352px] xs:w-full xs:object-cover"
-            />
+            <div className="sm:bg-[#F1F1F1] sm:h-352px sm:w-full sm:rounded-lg">
+              <Image
+                src={product.item.image.desktop}
+                alt={product.item.name}
+                width={540}
+                height={560}
+                // sizes="100vw"
+                className="w-[540px] h-[560px] rounded-xl xs:max-h-[352px] xs:w-full xs:object-cover sm:max-h-[352px] sm:w-full sm:object-contain md:max-h-[352px] md:min-w-[340px] md:object-contain md:rounded-lg lg:object-cover"
+              />
+            </div>
 
-            <div className="max-w-[445px] h-full gap-8 flex flex-col justify-center xs:items-center">
+            <div className="max-w-[445px] h-full gap-8 flex flex-col justify-center xs:items-center sm:items-center">
               {product.item.new && (
                 <h2 className="text-[#D87D4A] text-[14px] font-normal tracking-[10px] xs:text-center">
                   NEW PRODUCT
                 </h2>
               )}
-              <h1 className="text-black font-bold text-[40px] leading-[44px] tracking-[1.43px] xs:text-center xs:text-[28px] xs:leading-none xs:tracking-[1px]">
+              <h1 className="text-black font-bold text-[40px] leading-[44px] tracking-[1.43px] xs:text-center xs:text-[28px] xs:leading-none xs:tracking-[1px] sm:text-center">
                 {product.item.name}
               </h1>
-              <p className="text-[15px] leading-[25px] opacity-50 font-medium xs:text-center">
+              <p className="text-[15px] leading-[25px] opacity-50 font-medium xs:text-center sm:text-center">
                 {product.item.description}
               </p>
               <Link href={`${product.item.category}/${product.item.slug}`}>
@@ -84,12 +86,12 @@ const Category: React.FC<pageProps> = ({ params }) => {
         ))}
       </>
       {/* FEATURED ITEMS */}
-      <div className="pt-[160px] pb-[160px] xs:pt-[180px] xs:pb-[120px]">
+      <div className="pt-[160px] pb-[160px] xs:pt-[180px] xs:pb-[120px] sm:pt-[120px] sm:pb-[120px]">
         <FeatureItems />
       </div>
 
       {/* BESTGEAR */}
-      <div className="pb-[160px] xs:pb-[120px]">
+      <div className="pb-[160px] xs:pb-[120px] sm:pb-[120px]">
         <BestGear />
       </div>
     </div>
